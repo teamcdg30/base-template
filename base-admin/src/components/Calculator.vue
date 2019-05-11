5ee04e74cbb4082a0d5819b596503ee533a464cb
kanghoon.choi <kanghoon.choi@nhnent.com>
2019-05-11 12:21:35 +0900

    base-api 초기세팅

---
+++ base-admin/src/components/Calculator.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <input type="number" name="first" v-model="first" @keyup="calculate">
+    <input type="text" name="op" v-model="op" @keyup="calculate">
+    <input type="number" name="second" v-model="second" @keyup="calculate">
+    <input type="number" name="result" v-model="result" disabled>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    first: {
+      type: Number,
+      default: 0
+    },
+    second: {
+      type: Number,
+      default: 0
+    }
+  },
+  data() {
+    return {
+      result: 0,
+      op: '+'
+    }
+  },
+  mounted() {
+    this.calculate()
+  },
+  methods: {
+    calculate() {
+      this.result = this.first + this.second
+    }
+  }
+}
+</script>
--- base-admin/src/views/example/Example.vue
+++ base-admin/src/views/example/Example.vue
@@ -1,13 +1,74 @@
 <template>
   <div>
-    예제 모음
+    예제 모음 <br>
+    이름 : {{name}}<br>
+    나이 : {{age}}
+    <button @click="addAge(5)">나이 증가</button>
+
+    <calculator :first="10" :second="20"></calculator>
+
+    <li>
+      <ul v-for="member in filteredList" :key="member.name">
+        {{member.name}} / {{member.age}}
+      </ul>
+    </li>
   </div>
 </template>
 
 <script>
+import Calculator from '../../components/Calculator'
+
 export default {
+  data() {
+    return {
+      name: 'choi',
+      age: 34,
+      list: [
+        {
+          name: '이승무',
+          age: 30
+        },
+        {
+          name: '강성현',
+          age: 30
+        },
+        {
+          name: '김준하',
+          age: 31
+        },
+        {
+          name: '곽욱현',
+          age: 29
+        }
+      ]
+    }
+  },
   created() {
-    
+    console.log('생성 됨')
+  },
+  mounted() {
+    console.log('마운티드')
+  },
+  computed: {
+    filteredList() {
+      let result = this.list.filter(item => {
+        return item.age >= 30
+      })
+      return result
+    }
+  },
+  watch: {
+    age() {
+      console.log(this.age)
+    }
+  },
+  methods: {
+    addAge(num) {
+      this.age += num
+    }
+  },
+  components: {
+    Calculator
   }
 }
 </script>
